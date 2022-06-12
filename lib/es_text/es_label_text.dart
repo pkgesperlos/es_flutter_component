5195b9794c972616ef55b0c46ecb87be5353aadf
Hanieh <haniehhojjatzadeh@gmail.com>
2022-06-12 12:37:45 -0700

    Edit text components4

--- lib/es_text/es_label_text.dart
+++ lib/es_text/es_label_text.dart
@@ -23,11 +23,46 @@ class EsLableText extends StatelessWidget {
       data,
       textAlign: align,
       style: TextStyle(
+        color: color,
         fontSize: size,
-        fontFamily: "yekan",
+        fontFamily:fontFamily,
         fontWeight: FontWeight.w200,
         // overflow: TextOverflow.ellipsis,
       ),
     );
   }
 }
+
+
+// class EsTitle extends StatelessWidget {
+//   String data;
+//   TextAlign align;
+//   double size;
+//   Color color;
+//   String fontFamily;
+//
+//   EsTitle(
+//       {Key? key,
+//         required this.data,
+//         this.align = TextAlign.center,
+//         this.size = Constants.titleFontSize,
+//         this.color = Constants.titleText,
+//         this.fontFamily = Constants.fontFamily})
+//       : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       data,
+//       textAlign: align,
+//       style: TextStyle(
+//         color: color,
+//         fontSize: size,
+//         fontFamily:fontFamily,
+//         fontWeight: FontWeight.bold,
+//         // overflow: TextOverflow.ellipsis,
+//       ),
+//     );
+//   }
+// }
+
