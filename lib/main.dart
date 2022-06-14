39bfde30eb1ed1ab129f7fe6e4ebd83841f79f6f
Hanieh <haniehhojjatzadeh@gmail.com>
2022-06-14 13:48:33 -0700

    chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,12 @@
 // @dart=2.9
 
-import 'package:es_flutter_component/constants.dart';
-import 'package:es_flutter_component/es_button/es_loading_button.dart';
-import 'package:es_flutter_component/es_dialog/es_awesome_dialog.dart';
-import 'package:es_flutter_component/es_dialog/es_awesome_dialog/es_autohide_dialog.dart';
-import 'package:es_flutter_component/es_dialog/es_awesome_dialog/es_error_dialog.dart';
-import 'package:es_flutter_component/es_dialog/es_awesome_dialog/es_warning_dialog.dart';
-import 'package:es_flutter_component/es_form/es_animated_checkbox.dart';
-import 'package:es_flutter_component/es_form/es_checkbox_vallidation.dart';
 
-import 'package:es_flutter_component/es_form/es_date_time_picker/es_android_time_picker.dart';
-import 'package:es_flutter_component/es_form/es_date_time_picker/es_cupertino_time_picker.dart';
-
-import 'package:es_flutter_component/es_button/es_ordinary_button.dart';
-import 'package:es_flutter_component/es_form/es_date_time_picker/es_date_time_picker.dart';
-import 'package:es_flutter_component/es_form/es_file_picker.dart';
-import 'package:es_flutter_component/es_form/es_ordinary_slider.dart';
-import 'package:es_flutter_component/es_form/es_ranged_slider.dart';
-import 'package:es_flutter_component/es_form/es_text_editor.dart';
-import 'package:es_flutter_component/es_form/es_text_field.dart';
-import 'package:es_flutter_component/es_form/es_toggle_button.dart';
-import 'package:es_flutter_component/es_image/es_zooming_image.dart';
-import 'package:es_flutter_component/es_list/es_accardion_list.dart';
-import 'package:es_flutter_component/es_slider/es_carousel_slider.dart';
-import 'package:es_flutter_component/es_tab_navigator/es_side_navigator.dart';
-import 'package:es_flutter_component/es_table/es_editable_table.dart';
-import 'package:es_flutter_component/es_table/es_simple_table.dart';
-import 'package:es_flutter_component/es_text/es_dotted_text.dart';
-import 'package:es_flutter_component/es_text/es_label_text.dart';
-import 'package:es_flutter_component/es_text/es_marked_text.dart';
-import 'package:es_flutter_component/es_text/es_ordinary_text.dart';
-import 'package:es_flutter_component/es_text/es_title.dart';
-import 'package:es_flutter_component/test2.dart';
-import 'package:es_flutter_component/test3.dart';
-import 'package:es_flutter_component/test4.dart';
-import 'package:es_flutter_component/testhtml.dart';
+import 'package:es_flutter_component/es-chart/es-pie-chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'es_button/es_icon_button.dart';
-import 'es_dialog/es_awesome_dialog/es_body_dialog.dart';
-import 'es_dialog/es_awesome_dialog/es_info_dialog.dart';
-import 'es_dialog/es_awesome_dialog/es_success_dialog.dart';
-import 'es_form/es_checkbox.dart';
-import 'es_form/es_date_time_picker/es_cupertino_12h_time_picker.dart';
-import 'es_form/es_date_time_picker/es_persian_date_picker.dart';
-import 'es_form/es_drop_down_button.dart';
-import 'es_form/es_radio_button.dart';
-import 'es_form.dart';
-import 'es_notification/es_ordinary_notification.dart';
+import 'es-chart/es-circle_graph.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -97,9 +55,10 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             resizeToAvoidBottomInset: false,
 
-          body:Center(child: EsOrdinaryButton(text: 'press',onPressed: (){
-            NotificationApi.showNotification();
-          },),)
+          // body:Center(child: EsOrdinaryButton(text: 'press',onPressed: (){
+          //   NotificationApi.showNotification();
+          // },),)
+          body: Center(child: EsPieChart(),),
         ));
 
     
