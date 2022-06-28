162083de0f7720b7f63de64d3feaba0ecb6c34da
Hanieh <haniehhojjatzadeh@gmail.com>
2022-06-28 15:33:59 -0700

    Text and button edit2

--- lib/es_form/es_text_field/es_text_field.dart
+++ lib/es_form/es_text_field/es_text_field.dart
@@ -1,6 +1,6 @@
 import 'package:es_flutter_component/images/Constants/dims.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 ///this class is a customized text field that use in whole of app
 class EsTextField extends StatefulWidget {
   String? hint = "";
@@ -8,78 +8,56 @@ class EsTextField extends StatefulWidget {
   TextEditingController tec = new TextEditingController();
   TextAlign? textAlign;
   int? maxLength;
-  int? maxLines = 1;
-  bool? checkRegex = false;
-  bool? border = false;
+  int maxLines = 1;
+  bool checkRegex = false;
+  bool border;
+  bool obscure;
+  bool repeatedPassword;
   TextEditingController? controller = TextEditingController();
   ValueChanged<String>? onChanged;
+  String Function(String value)? checkRepeat;
+  FocusNode? focusNode;
+  FocusNode? nextFocusNode;
 
-  EditTextController? editTextController = EditTextController();
-
-  EsTextField({this.hint});
-
-  EsTextField.withInput({this.hint, this.textInput, this.textAlign});
-
-  EsTextField.withMaxLength(
-      {this.hint, this.textInput, this.textAlign, this.maxLength});
-
-  EsTextField.checker(
-      {this.hint,
-      this.textInput,
-      this.textAlign,
-      this.maxLength,
-      this.controller,
-      this.onChanged,
-      this.editTextController,
-      this.checkRegex})
-      : assert(editTextController != null);
-
-  EsTextField.text(
-      {this.hint,
-      this.textInput,
-      this.textAlign,
-      this.maxLength,
-      this.controller,
-      this.onChanged,
-      this.maxLines,
-      this.checkRegex,
-      this.border = false});
-
-
-  EsTextField.form(
-      {this.hint,
-        this.textInput,
-        this.textAlign,
-        this.maxLength,
-        this.controller,
-        this.onChanged,
-        this.maxLines,
-        this.border,
-        this.checkRegex
-      });
+  EsTextField({
+    this.hint,
+    this.textInput,
+    this.textAlign,
+    this.maxLength,
+    this.controller,
+    this.onChanged,
+    this.maxLines = 1,
+    this.repeatedPassword = false,
+    this.border = false,
+    this.checkRepeat,
+    this.checkRegex = false,
+    this.obscure = false,
+    this.focusNode,
+    this.nextFocusNode,
+  });
 
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return _EsTextField();
+    return _EsTextFieldFormState();
   }
 }
 
-class _EsTextField extends State<EsTextField> {
-  Color borderColor = Colors.black54;
-
+class _EsTextFieldFormState extends State<EsTextField> {
+  late String Function(String value) _checkRepeat;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _checkRepeat = widget.checkRepeat!;
+  }
 
-    if(widget.checkRegex == null){
-      widget.checkRegex = false;
-    }
-    if(widget.border == null){
-      widget.border = false;
-    }
+  @override
+  void didUpdateWidget(EsTextField oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    _checkRepeat = widget.checkRepeat!;
   }
 
   @override
@@ -87,106 +65,63 @@ class _EsTextField extends State<EsTextField> {
     // TODO: implement build
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: TextField(
+      child: TextFormField(
+        focusNode: widget.focusNode,
         maxLength: widget.maxLength,
         controller: widget.controller,
-        onChanged: checkChange,
+        onChanged: widget.onChanged,
         keyboardType: widget.textInput,
         maxLines: widget.maxLines,
-        textAlign:
-        widget.textAlign ?? TextAlign.right ,
+        obscureText: widget.obscure,
+        onFieldSubmitted: (v) {
+          if (widget.nextFocusNode != null)
+            FocusScope.of(context).requestFocus(widget.nextFocusNode);
+        },
+        textAlign: widget.textAlign ?? TextAlign.right,
         style: new TextStyle(fontSize: Dims.h3FontSize(context)),
-        decoration: new InputDecoration(
-          focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: borderColor),
-              borderRadius:
-              BorderRadius.all(Radius.circular(Dims.h2BorderRadius(context)))),
-          contentPadding: EdgeInsets.all(Dims.h3Padding(context)),
-          alignLabelWithHint: false,
-          border: OutlineInputBorder(
-              borderRadius:
-              BorderRadius.all(Radius.circular(Dims.h2BorderRadius(context)))),
-          labelText: widget.hint,
-          labelStyle: TextStyle(fontSize: Dims.h3FontSize(context)),
-        ),
+        decoration: decoration(),
       ),
     );
   }
 
-  RegExp phoneReg = new RegExp(
-    "(09)[0-9]{9}",
-    caseSensitive: false,
-    multiLine: false,
-  );
-  RegExp emailReg = new RegExp(
-    r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-    caseSensitive: false,
-    multiLine: false,
-  );
 
-  checkChange(String value) {
+  decoration() {
+    bool isObscure = false;
+    if (widget.hint == "رمز عبور" || widget.hint == "تکرار رمز عبور")
+      isObscure = true;
 
-    if(widget.onChanged != null) widget.onChanged!(value);
-    if (value.length > 0) {
-      setState(() {
-        borderColor = Colors.red;
-
-        widget.editTextController?.isAccepted = false;
-      });
-
-      checkSuccess(value);
+    if (isObscure) {
+      return InputDecoration(
+          contentPadding: EdgeInsets.only(right: 8),
+          suffixIcon: InkWell(
+            onTap: () {
+              // Update the state i.e. toogle the state of passwordVisible variable
+              setState(() {
+                widget.obscure = !widget.obscure;
+              });
+            },
+            child: Icon(
+                widget.obscure ? Icons.visibility : Icons.visibility_off,
+                color: Colors.black38),
+          ),
+          alignLabelWithHint: true,
+          border: widget.border
+              ? OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)))
+              : null,
+          labelText: widget.hint,
+          labelStyle: TextStyle(fontSize: 16));
     } else {
-      setState(() {
-        borderColor = Colors.black54;
-
-        widget.editTextController?.isAccepted = false;
-      });
+      return new InputDecoration(
+          alignLabelWithHint: true,
+          border: widget.border
+              ? OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)))
+              : null,
+          labelText: widget.hint,
+          labelStyle: TextStyle(fontSize: 16),
+          contentPadding:
+              EdgeInsets.only(left: 8, right: 8, bottom: 12, top: 12));
     }
-  }
-
-  void checkSuccess(String value) {
-    if (widget.checkRegex!) {
-      if (widget.textInput == TextInputType.phone && phoneReg.hasMatch(value)) {
-        setAccept();
-      } else if (widget.textInput == TextInputType.emailAddress &&
-          emailReg.hasMatch(value)) {
-        setAccept();
-      } else {
-        setState(() {
-          widget.editTextController?.isAccepted = false;
-        });
-      }
-    } else {
-      if (widget.maxLength != null) {
-        print(widget.maxLength);
-
-        if (value.length == widget.maxLength) {
-          setAccept();
-        } else {
-          setState(() {
-            widget.editTextController?.isAccepted = false;
-          });
-        }
-      } else {
-        setAccept();
-      }
-    }
-  }
-
-  setAccept() {
-    setState(() {
-      borderColor = Colors.green;
-      widget.editTextController?.isAccepted = true;
-    });
-  }
-}
-
-class EditTextController {
-  bool _isAccepted = false;
-
-  bool get isAccepted => _isAccepted;
-
-  set isAccepted(bool value) {
-    _isAccepted = value;
   }
 }
