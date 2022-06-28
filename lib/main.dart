9c251eb5b237f27f007adf7ed4161c144b37f766
sajjad.ws <sajjad.ws@gmail.com>
2022-06-28 10:55:50 +0430

    es_responsive_table created

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:es_flutter_component/es_text/es_ordinary_text.dart';
 import 'package:es_flutter_component/es_text/es_paragraph.dart';
 import 'package:es_flutter_component/es_text/es_subtitle.dart';
 import 'package:es_flutter_component/es_text/es_title.dart';
-import 'package:es_flutter_component/test.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
