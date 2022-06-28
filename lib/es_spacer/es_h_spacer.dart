41caac9eed4abdb9aa1d26becfeca3b5ef148560
Hanieh <haniehhojjatzadeh@gmail.com>
2022-06-28 05:28:39 -0700

    add es_spacer.dart

---
+++ lib/es_spacer/es_h_spacer.dart
@@ -0,0 +1,21 @@
+import 'package:es_flutter_component/images/Constants/dims.dart';
+import 'package:flutter/material.dart';
+
+
+class EsHSpacer extends StatelessWidget{
+
+  final bool big;
+  final int factor;
+
+
+
+  EsHSpacer({this.big = false,this.factor=1});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(
+      width: big?factor*Dims.h1Padding(context):factor*Dims.h2Padding(context),
+    );
+  }
+}
---
+++ lib/es_spacer/es_v_spacer.dart
@@ -0,0 +1,20 @@
+import 'package:es_flutter_component/images/Constants/dims.dart';
+import 'package:flutter/material.dart';
+
+
+class EsVSpacer extends StatelessWidget{
+
+  final bool big;
+  final int factor;
+
+
+  EsVSpacer({this.big = false,this.factor=1});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(
+      height: big?factor*Dims.h1Padding(context):factor*Dims.h2Padding(context),
+    );
+  }
+}
