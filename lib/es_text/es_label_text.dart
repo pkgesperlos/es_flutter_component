f61047e1b2a9a3b0aea614c49c7cf2ef955b7dd2
Hanieh <haniehhojjatzadeh@gmail.com>
2022-06-29 08:31:41 -0700

    Merge branch 'haniehj' of github.com:pkgesperlos/es_flutter_component
    # Conflicts:
    #	lib/test.dart

--- lib/es_text/es_label_text.dart
+++ lib/es_text/es_label_text.dart
@@ -2,11 +2,12 @@
 import 'package:flutter/material.dart';
 
 import '../images/Constants/constants.dart';
+import '../images/Constants/dims.dart';
 
 class EsLabelText extends StatelessWidget {
   String data;
   TextAlign align;
-  double size;
+  double? size;
   Color color;
   String fontFamily;
   bool isBold;
@@ -14,7 +15,7 @@ class EsLabelText extends StatelessWidget {
   EsLabelText(this.data,
       {Key? key,
         this.align = TextAlign.center,
-        this.size = Constants.labelFontSize,
+        this.size ,
         this.color = Constants.labelText,
         this.fontFamily = Constants.fontFamily,
         this.isBold=false
@@ -23,6 +24,7 @@ class EsLabelText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    defaults(context);
     return Text(
       data,
       textAlign: align,
@@ -35,38 +37,9 @@ class EsLabelText extends StatelessWidget {
       ),
     );
   }
+  void defaults(context) {
+    size=Dims.h1FontSize(context);
+  }
 }
 
-
-// class EsTitle extends StatelessWidget {
-//   String data;
-//   TextAlign align;
-//   double size;
-//   Color color;
-//   String fontFamily;
-//
-//   EsTitle(
-//       {Key? key,
-//         required this.data,
-//         this.align = TextAlign.center,
-//         this.size = Constants.titleFontSize,
-//         this.color = Constants.titleText,
-//         this.fontFamily = Constants.fontFamily})
-//       : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Text(
-//       data,
-//       textAlign: align,
-//       style: TextStyle(
-//         color: color,
-//         fontSize: size,
-//         fontFamily:fontFamily,
-//         fontWeight: FontWeight.bold,
-//         // overflow: TextOverflow.ellipsis,
-//       ),
-//     );
-//   }
-// }
 
