4bf4dde66fc85db20a9901c28f457151e60e1494
Hanieh <haniehhojjatzadeh@gmail.com>
2022-07-06 14:16:17 -0700

    add es_simple_model_drop_down.dart

---
+++ lib/es_form/es_drop_down_button/es_simple_model_drop_down.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class EsSimpleModelDropDown extends StatefulWidget {
+  String value;
+  String initialTitle;
+  String idName;
+  String valueName;
+  List? list;
+  Function(String id)? onChange;
+
+  EsSimpleModelDropDown(
+      {
+        this.value = "",
+        this.list,
+        this.onChange,
+        this.idName = "_id",
+        this.valueName = "title",
+        this.initialTitle = "انخاب کتید"});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _EsSimpleModelDropDownState();
+  }
+}
+
+class _EsSimpleModelDropDownState extends State<EsSimpleModelDropDown> {
+  String _value = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _value = widget.value;
+
+    widget.list!
+        .insert(0, {widget.idName: "", widget.valueName: widget.initialTitle});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return DropdownButton(
+        isExpanded: true,
+        value: _value,
+        items: widget.list!
+            .map((model) => DropdownMenuItem(
+          value: model[widget.idName],
+          child: Text(model[widget.valueName]),
+        ))
+            .toList(),
+        onChanged: (value) {
+          setState(() {
+            _value = value.toString();
+
+            widget.onChange!(value.toString());
+          });
+        });
+  }
+
+  @override
+  void didUpdateWidget(covariant EsSimpleModelDropDown oldWidget) {
+    _value = widget.value;
+    super.didUpdateWidget(oldWidget);
+  }
+}
--- lib/test.dart
+++ lib/test.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:es_flutter_component/es_button/es_button.dart';
+
 import 'package:es_flutter_component/es_form/es_radio_button.dart';
 import 'package:es_flutter_component/resources/Constants/dims.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +24,9 @@ class Test extends StatelessWidget {
         child: Column(
           children: [
             IntrinsicWidth(child: EsRadioButton(titleList: ["num1","num2"],horizontal: true,)),
+            // EsBorderedDropDownButton( items: ["دسته بندی2", "1دسته بندی"],
+            //
+            //     onTapItems: [() {}, () {}]),
             // EsSvgIcon(
             //   'assets/images/HistoryOutlined.svg',
             //   color: Colors.black,
